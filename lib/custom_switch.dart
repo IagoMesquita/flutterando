c508f451ec8791452636b1541dc1fe996fcefe1e
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-04-22 18:26:38 -0300

    feat: implement AnimatedBuilder

--- lib/custom_switch.dart
+++ lib/custom_switch.dart
@@ -1,24 +1,27 @@
-// import 'package:flutter/material.dart';
-// import 'app_controller.dart';
+import 'package:flutter/material.dart';
+import 'app_controller.dart';
 
-// class CustomSwitch extends StatelessWidget {
-//   const CustomSwitch({super.key});
+class CustomSwitch extends StatelessWidget {
+  const CustomSwitch({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Switch(
-//       value: !AppController.instance.isDartTheme,
-//       onChanged: (value) {
-//         AppController.instance.changeTheme();
-//         print('Value: $value');
-//         print('isDartTheme: ${AppController.instance.isDartTheme}');
-//       },
-//       // value: isDartTheme,
-//       // onChanged: (value) {
-//       //   setState(() {
-//       //     isDartTheme = value;
-//       //   });
-//       // },
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: AppController.instance,
+      builder: (context, child) {
+        return Switch(
+          value: AppController.instance.isDartTheme,
+          onChanged: (value) {
+            AppController.instance.changeTheme();
+          },
+          // value: isDartTheme,
+          // onChanged: (value) {
+          //   setState(() {
+          //     isDartTheme = value;
+          //   });
+          // },
+        );
+      },
+    );
+  }
+}
